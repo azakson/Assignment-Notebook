3457a5d06c0ca18cd4f1f2681aa8fe623d9119b9
Avery <Zakson>
2020-01-16 10:54:34 -0600

    Added TaskManager cocoa touch class

---
+++ Assignment Notebook/TaskManager.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManager.swift
+//  Assignment Notebook
+//
+//  Created by Avery Zakson on 1/16/20.
+//  Copyright © 2020 mojo. All rights reserved.
+//
+
+import UIKit
+
+class TaskManager: Codable {
+   
+    var subject: String
+    var dueDate: String
+    var description: String
+    
+}
